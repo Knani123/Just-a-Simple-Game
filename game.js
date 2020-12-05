1823a13d7e4a416ae9f46ee12b4b9eb848d4d088
Knani123 <knani.yosri@yahoo.com>
2020-12-05 14:45:58 +0100

    3 commit

--- game.js
+++ game.js
@@ -2,6 +2,11 @@
 let friend=document.querySelector('.friend')
 let enemy=document.querySelector('.enemy')
 let block=document.querySelector('.block')
+ 
+//create compteur
+let compteur=document.createElement("p")
+compteur.classList.add('c')
+console.log(compteur)
 
 // nigth and day
 let bg=true
@@ -11,27 +16,33 @@ const mode=()=>{
 chageTime=setInterval(() => {
         bg=!bg
         console.log(bg)
-        if(bg){block.style.backgroundColor="white"}
-        else{block.style.backgroundColor="black"}
+        if(bg){block.style.backgroundColor="white";compteur.style.color="black"}
+        else{block.style.backgroundColor="black";compteur.style.color="white"}
      }, 4000);
     
 }
+
+
 
 //create btn 
 let btn=document.createElement('button')
 btn.innerText="play"
 btn.classList.add("replay")
+
 btn.addEventListener("click",(e)=>{
     mode()
+    block.appendChild(compteur)
+    compteur.innerHTML=0;
+    compteur.style.color="black"
     friend.style.left="0";
     enemy.style.left="480px"
     block.style.backgroundColor="white"
     enemy.style.animation="attac 2s infinite linear" 
     e.target.style.visibility="hidden"
     block.removeChild(lose)
-   
      
 })
+
 block.appendChild(btn)
 
 // Create Text
@@ -42,6 +53,7 @@ let lose=document.createElement("p")
 // function to jump
 const jump=(e)=>{
    if(e.keyCode==32){
+      
     friend.classList.add("annimate")
     setTimeout(() => {
         friend.classList.remove("annimate")
@@ -54,8 +66,10 @@ document.addEventListener('keypress',jump)
  let checkDead=setInterval(() => {
     let friendTop=window.getComputedStyle(friend).getPropertyValue("top")
     let enemyLeft=window.getComputedStyle(enemy).getPropertyValue("left")
-  
- if(parseInt(enemyLeft)>=0 && parseInt(enemyLeft)<=20 && parseInt(friendTop)>130 ){
+    if(parseInt(enemyLeft)==0){
+      compteur.innerHTML=Number(compteur.innerHTML)+1
+    }
+ else if(parseInt(enemyLeft)>0 && parseInt(enemyLeft)<=20 && parseInt(friendTop)>130 ){
     enemy.style.animation="none"
     block.appendChild(lose)
     alert("you lose")
@@ -63,10 +77,10 @@ document.addEventListener('keypress',jump)
     btn.style.visibility="visible"
  clearInterval(chageTime)
  block.style.backgroundColor="black"
+ compteur.style.color="white"
  friend.style.left="50%";
  enemy.style.left="45%"
  }
-
- }, 10);
+ }, 14);
 
 
